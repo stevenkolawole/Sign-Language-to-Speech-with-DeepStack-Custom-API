3900fee4c17b01f03ecd12f70ec5322dfb0f93e1
stevekola <kolawolesteven99@gmail.com>
2021-06-29 18:53:02 +0100

    create dataset via webcam

--- creating_data.py
+++ creating_data.py
@@ -1,12 +1,13 @@
-import cv2, os, time, uuid
+import cv2, os, time
 
 IMAGES_PATH = 'dataset'
 try:
     os.mkdir(IMAGES_PATH)
 except OSError:
     pass
-labels = ['hello', 'thanks', 'yes', 'no', 'iloveyou']
-NUMBER_IMGS = 3
+labels = [ 'nice to meet you', 'hello', 'thanks', 'yes', 'no',
+           'iloveyou', 'please', 'sorry', 'you\'re welcome',]
+NUMBER_IMGS = 15
 
 for label in labels:
     try:
@@ -15,13 +16,13 @@ for label in labels:
         pass
     capture = cv2.VideoCapture(0)
     print(f'Collecting images for {label}')
-    time.sleep(5)
+    time.sleep(15)
     for image_num in range(NUMBER_IMGS):
         ret, frame = capture.read()
-        image_name = os.path.join(IMAGES_PATH, label, f'{label}_{uuid.uuid1()}.jpg')
+        image_name = os.path.join(IMAGES_PATH, label, f'{label}_{image_num}.jpg')
         cv2.imwrite(image_name, frame)
         cv2.imshow('frame', frame)
-        time.sleep(4)
+        time.sleep(3)
         
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
